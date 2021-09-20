bf60ac9249bab834bd745a525b95db4d5a830f5b
pottfisk <robert.paananen@hotmail.com>
2021-09-20 18:28:13 +0200

    Function that finds all possible moves and creates a new state for each one

--- src/game_engine.py
+++ src/game_engine.py
@@ -1,6 +1,6 @@
 from board import Board
 from reader import Reader
-
+import copy
 class Engine(object):
     """
     Engine class
@@ -17,16 +17,18 @@ class Engine(object):
     Color should be either 'white' or 'black'
     Example: move_piece((1,1),(1,2),"white") returns
     """
-    def move_piece(self, start, end, player_color):
+    def move_piece(self, start, end, player_color, lines = None):
+        if not lines: lines = self._board.get_lines()
         start_x, start_y = start
         end_x, end_y = end
-        lines = self._board.get_lines()
         list_of_starts = []
         list_of_ends = []
         for line in lines:
             start_position = next((item for item in line if item["xy"] == [start_x,start_y]), None)
             end_position = next((item for item in line if item["xy"] == [end_x,end_y]), None)
+            #print(start_position)
             if start_position and start_position['owner'] == "none":
+                print("here")
                 return -1  #Returns error code -1 if move is illegal
             elif start_position:
                 list_of_starts.append(start_position) 
@@ -42,7 +44,7 @@ class Engine(object):
             end["owner"] = player_color
         for start in list_of_starts:
             start['owner'] = "none"
-
+        return lines
 
     """
     Arguments: list of lists containing the lines for a board, player color
@@ -78,6 +80,8 @@ class Engine(object):
             all_states.append(new_lines)
         return all_states
 
+
+
     """
     Arguments: list of lists containing the lines for a board, player color
     Returns: A list of lists of lists containing every possible move of a piece as a seperate game state.
@@ -87,8 +91,38 @@ class Engine(object):
     def all_possible_states_for_move(self, lines= None, player_color = None):
         if not lines: lines = self._board.get_lines()
         if not player_color: player_color = self._board.get_player_turn()
-
-        pass
+        lines_local = copy.deepcopy(lines)
+        all_states = []
+        for line in lines_local:
+            for index, item in enumerate(line):
+                if(item['owner'] == player_color):
+                    
+                    if index != 0 and index != len(line)-1:
+                        before = line[index - 1]
+                        after = line[index + 1]
+                        if(before['owner'] == 'none'):
+                            ret_val = self.move_piece(tuple(item['xy']),tuple(before['xy']),player_color,copy.deepcopy(lines_local))
+                            if ret_val == -1: return ret_val
+                            all_states.append(ret_val)
+                        if(after['owner'] == 'none'):                            
+                            all_states.append(self.move_piece(tuple(item['xy']),tuple(after['xy']),player_color,copy.deepcopy(lines_local)))
+                    elif index == 0 and len(line) > 1:
+                        after = line[index + 1]
+                        if(after['owner'] == 'none'):
+                            ret_val = self.move_piece(tuple(item['xy']),tuple(after['xy']),player_color,copy.deepcopy(lines_local))
+                            if ret_val == -1: return ret_val
+                            all_states.append(ret_val)
+                    elif index == len(line) -1:
+                        before = line[index - 1]
+                        if(before['owner'] == 'none'):
+                            ret_val = self.move_piece(tuple(item['xy']),tuple(before['xy']),player_color,copy.deepcopy(lines_local))
+                            if ret_val == -1: return ret_val
+                            all_states.append(ret_val)
+                            
+                        
+        return all_states
+            
+            #(item for item in line if item['owner'] == player_color)
 
 
 def main():
