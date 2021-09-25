d87233898a836d5fdb734d9fba0fb6a062d89e89
Jennifer Gross <36174212+soothingjennyg@users.noreply.github.com>
2021-09-25 17:57:20 +0200

    fixed heuristics
    The heuristics should be a good starting point to test.

--- src/heuristic.py
+++ src/heuristic.py
@@ -2,7 +2,6 @@ import unittest
 import sys
 import os
 import json
-#import numpy as np
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 import reader as file_reader
 from game_engine import Engine
@@ -19,12 +18,15 @@ from board import Board
  #       :param: board_size (int)
  #       :param: lines (list(list))
 
-#going to analyze everything with the more positive number going to black; negative going to better for white.
+#going to analyze everything with the more positive number going to black; 
+#negative going to better for white.
+
 class Heuristic():
-#counts each point on the board where a piece can be played and counts them. Where the array is > 1 there are 2 possible lines.
+
+    #I originally planned to weight things with arrasy but decided it may not be needed. TBD
     def createVertexArray(board):
-        # vertexArray = np.zeros(board.board_size,board.board_size)
-        print(board)
+      
+       # print(board)
         #vertexArray = [board.get_board_size(),board.get_board_size()]
         #myArray = [board.get_board_size(),board.get_board_size()]
         w, h = board.get_board_size(), board.get_board_size()
@@ -36,32 +38,49 @@ class Heuristic():
                 myArray[x][y] += 1
         return myArray
 
-    #TODO: this requires more work to make a good heuristic
+ #counts all the blacks and whites.  
+ #When there is a vertex, the values will get counted for each one, giving them a heavier weight. 
+# Also gives +/- 3 for having 3 in a row.
     def firstPhaseState(self, board):
-        vertex = Heuristic.createVertexArray(board)
+        score = 0
+        for line in board._lines:
+            numinrow =0
+            last = ""
+            for item in line:
+                if ( last == item["owner"]):
+                    numinrow +=1
+                else :
+                    numinrow = 0
+                if item["owner"] == "black":
+                    score = score +1
+                    if numinrow == 3:
+                        score = score + 3
+                elif item["owner"] == "white":
+                    score = score - 1
+                    if numinrow == 3:
+                        score = score - 3
+                last = item["owner"]     
+        return score
 
-        value = board.get_black_pieces_left() - board.get_white_pieces_left()
-        return value
 
-
-    #TODO: This is more of a marker than a good function at this point, need to create heuristic
+    #if a win condition is met, gives a score of =10/-10
+    #else score is a delta between types of pieces
     def secondPhaseState(self, board):
-        value = board.get_black_pieces_left() - board.get_white_pieces_left()
-        return value
-
-
-    def pieceDifference(self, board):
-        value = board.get_black_pieces_left - board.get_white_pieces_left
-        return value
-
+        score = board.get_black_pieces_left() - board.get_white_pieces_left()
+        if board.get_black_pieces_left() == 2:
+            score = -10
+        if board.get_white_pieces_left() == 2:
+            score = +10
+        return score
 
 
 
-    def createPlayerArray(self, board):
+   # def createPlayerArray(self, board):
      #   playerArray = np.zeros(board.board_size, board.board_size)
-        print("This is a print statement, please print")
 
 
-
+#board = Board("low","black",12,12,12,12,24,[
+#    [{"xy":[1,1], "owner": "black"},{"xy":[1,2], "owner":"none"},{"xy":[1,3], "owner":"none"}],
+#    [{"xy":[1,1], "owner": "black"},{"xy":[2,1], "owner":"none"},{"xy": [3,1], "owner": "none"}] ])
 
 
