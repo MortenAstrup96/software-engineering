3bee2dfb23ce2b2c7d0aaf045af9ea4391a840ce
Laurent Vouriot <laurent.vouriot@etu.univ-amu.fr>
2021-09-27 17:22:23 -0400

    test_reader.py added test for read and write functions

--- test/test_reader.py
+++ test/test_reader.py
@@ -4,7 +4,9 @@ reader_tests.py
 Software-engineering project group B
 
 14/09/2021
-v0.1
+v0.2
+
+last update : 27/09/21
 
 Laurent VOURIOT
 """
@@ -18,9 +20,9 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 import reader as file_reader
 
 class TestReader(unittest.TestCase):
-    def test_reader(self):
+    
+    def test_read(self):
         reader = file_reader.Reader()
-
         reader.read("../src/test.json")
         board = reader.board
 
@@ -38,14 +40,49 @@ class TestReader(unittest.TestCase):
             [{"xy":[3,1], "owner": "none"},{"xy":[4,1], "owner":"none"},{"xy": [5,1], "owner": "none"}]
             ])
 
-
-
-
         # errors
         with self.assertRaises(Exception):
             reader.read("fddsfsdfsdf")
 
+    def test_write(self):
+        reader = file_reader.Reader()
+        reader.read("../src/test.json")
+        board = reader.board
+        # maybe use the constructor and test setters in another function 
+        # values make no sense just to test if setting and writing works
+        board.set_difficulty("high")
+        board.set_turn_number(1)
+        board.set_player_turn("Black")
+        board.set_white_pieces_in_hand(11)
+        board.set_black_pieces_in_hand(11)
+        board.set_white_pieces_left(10)
+        board.set_black_pieces_left(9)
+        board.set_board_size(48)
+        board.set_lines([
+            [{"xy":[1,3], "owner": "white"},{"xy":[1,5], "owner":"white"},{"xy":[4,3], "owner":"none"}],
+            [{"xy":[6,1], "owner": "black"},{"xy":[2,2], "owner":"black"},{"xy": [3,1], "owner": "none"}],
+            [{"xy":[3,1], "owner": "none"},{"xy":[4,1], "owner":"none"},{"xy": [2,8], "owner": "black"}]
+            ])
 
+        reader.write("./test_write.json")
+
+        
+        # we read the freshly written json file and verify the values
+        reader.read("./test_write.json")
+        board = reader.board
+        self.assertEqual(board.get_difficulty(), 'high')
+        self.assertEqual(board.get_turn_number(), 1)
+        self.assertEqual(board.get_player_turn(), "Black")
+        self.assertEqual(board.get_white_pieces_in_hand(), 11)
+        self.assertEqual(board.get_black_pieces_in_hand(), 11)
+        self.assertEqual(board.get_white_pieces_left(), 10) 
+        self.assertEqual(board.get_black_pieces_left(), 9)
+        self.assertEqual(board.get_board_size(), 48)
+        self.assertEqual(board.get_lines(), [
+            [{"xy":[1,3], "owner": "white"},{"xy":[1,5], "owner":"white"},{"xy":[4,3], "owner":"none"}],
+            [{"xy":[6,1], "owner": "black"},{"xy":[2,2], "owner":"black"},{"xy": [3,1], "owner": "none"}],
+            [{"xy":[3,1], "owner": "none"},{"xy":[4,1], "owner":"none"},{"xy": [2,8], "owner": "black"}]
+            ])
             
 if __name__ == '__main__':
     unittest.main()
